cb6c40ca9e79492848307efea98e8946c5959e9b
VisionArc <visionarc.chemnitz@gmail.com>
2025-02-06 09:51:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ origins = [
 # cors middleware
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=[*],
+  allow_origins=["*"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
